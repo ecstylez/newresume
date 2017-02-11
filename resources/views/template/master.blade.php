3c0f726f78042e63b564d1bd851ef74dd69f66b5
vagrant <vagrant@homestead>
2017-02-11 19:36:20 +0000

    adding back until it is verified that the dns is pointed correctly

--- resources/views/template/master.blade.php
+++ resources/views/template/master.blade.php
@@ -19,7 +19,7 @@
 					</hgroup>
 					<article>
 						<p>
-							My name is Mark Eccleston, and I am a software engineer. PHP Development and Web Design have been an essential part of my life for the past 15+ years. I've worked with many types of companies, from startups, to full fledged organizations; From being a one man army to being a part of an expansive development team; From being solely a web designer to being a full on software developer. Being a part of something big and learning as I go has always been what intrigues me most. Over time, I've established core skills in HTML, CSS3, Sass, Javascript (jQuery, Prototype), SQL, and more, working under Agile development environments. I'm currently based in the Fort Lauderdale, FL area. <a href="/about-me" title="Find out more about Mark Eccleston, the PHP Developer">Would you like to know more?</a>
+							PRODUCTION. My name is Mark Eccleston, and I am a software engineer. PHP Development and Web Design have been an essential part of my life for the past 15+ years. I've worked with many types of companies, from startups, to full fledged organizations; From being a one man army to being a part of an expansive development team; From being solely a web designer to being a full on software developer. Being a part of something big and learning as I go has always been what intrigues me most. Over time, I've established core skills in HTML, CSS3, Sass, Javascript (jQuery, Prototype), SQL, and more, working under Agile development environments. I'm currently based in the Fort Lauderdale, FL area. <a href="/about-me" title="Find out more about Mark Eccleston, the PHP Developer">Would you like to know more?</a>
 						</p>
 					</article>
 				</div>
